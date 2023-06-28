cbd923c3f5db7b476ac4e9267615fc1b0822c7e5
kwiyoon <jiyun0245@gmail.com>
2023-06-28 16:35:55 +0900

    chore: @SpringBootTest 주석 처리 (EC2에서 gradlew build 불가)

--- src/test/java/kuit/server/KuitServerApplicationTests.java
+++ src/test/java/kuit/server/KuitServerApplicationTests.java
@@ -3,7 +3,7 @@ package kuit.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class KuitServerApplicationTests {
 
     @Test
