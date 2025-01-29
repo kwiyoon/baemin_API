d47ca70e5ec9d16914755fa9184fe10c573cfb89
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-01-30 08:17:38 +0900

    fix: jar 위치 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # JAR 파일명을 deploy.log에 기록.
-BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/zzoni/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
