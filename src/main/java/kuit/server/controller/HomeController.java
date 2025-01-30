1609c74eba257b46378031e64abc2bd2f05993f9
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-01-30 09:13:30 +0900

    test: 배포 테스트

--- src/main/java/kuit/server/controller/HomeController.java
+++ src/main/java/kuit/server/controller/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
   @GetMapping("/home")
     public String home() {
-        return "TEST!!!";
+        return "TEST2!!!";
     }
 }
